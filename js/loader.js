1e60a1ee36136a2aa4e5b0c3d4d33a7abf2a30c3
HudsonJr <76413841+Hudsonjr90@users.noreply.github.com>
2021-08-27 19:48:24 -0300

    change files

--- js/loader.js
+++ js/loader.js
@@ -7,18 +7,18 @@ var divPizzas = document.querySelector('.divPizzas');
 
 function load () {
 
-    if(window.onload = true) {
+    if(window.onload == true) {
         divCarga.style.top = '0px';
 
-    }if(window.onload = true) {
+    }if(window.onload == true) {
         divCarga.style.top = '-100000px';
         
-    };
-};
+    }
+}
 
 setTimeout(function() {
     divCarga.style.top = '-100000px';
-},3000)
+},3000);
 
 
 
